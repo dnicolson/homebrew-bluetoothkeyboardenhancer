6b6cb0b7d676041b679293910471b629003ebff9
Dave Nicolson <david.nicolson@gmail.com>
2026-02-22 19:05:27 +0100

    Fix brew service

--- bluetoothkeyboardenhancer.rb
+++ bluetoothkeyboardenhancer.rb
@@ -9,23 +9,9 @@ class Bluetoothkeyboardenhancer < Formula
     bin.install "bke"
   end
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-        <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{bin}/bke</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
-    EOS
+  service do
+    run [opt_bin/"bke"]
+    keep_alive true
+    run_at_load true
   end
 end
